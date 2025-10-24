a920b17df90b5ac9d9e6133be1874905e61571c2
Chandan <chandan.24bcs10105@sst.scaler.com>
2025-10-24 22:47:37 +0530

    frontend

--- frontend/src/api/movieApi.js
+++ frontend/src/api/movieApi.js
@@ -5,9 +5,10 @@ import axios from "axios";
 const baseURL = import.meta.env.VITE_API_URL || '/api';
 
 const API = axios.create({
-  baseURL,
+  baseURL: 'https://movie-wishlist-39p5.onrender.com/api',
   headers: { "Content-Type": "application/json" },
 });
+
 
 export const getWishlist = () => API.get("/movies").then((res) => res.data);
 export const addMovie = (movie) => API.post("/movies", movie).then((res) => res.data);
